524b908e3aa8466d9045423dfd68d38ec2c041b0
Jevons <tmoonlee@qq.com>
2020-08-25 18:22:50 +0800

    publish: 1.0.2

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    root: true,
-    extends: '@react-native-community',
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    indent: ["error", "tab"]
-};
